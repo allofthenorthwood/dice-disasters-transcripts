af18f0217a7662893eac03630bf50c4c6aed3b66
Oliver Northwood <allofthenorthwood@gmail.com>
2020-06-19 17:20:26 -0700

    Make it so the iframe resizes uwu

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import "./index.css";
 import App from "./App";
 // import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("dice-disasters-transcript-app"));
+let container = document.getElementById("dice-disasters-transcript-app");
+ReactDOM.render(<App />, container);
+
+if (window.parent !== window) {
+  let lastHeight = -1;
+  function check() {
+    let height = container.getBoundingClientRect().height;
+    if (height !== lastHeight) {
+      lastHeight = height;
+      parent.postMessage({height}, '*');
+    }
+  }
+  let observer = new MutationObserver(check);
+  observer.observe(container, { attributes: true, childList: true, subtree: true });
+  requestAnimationFrame(check);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
